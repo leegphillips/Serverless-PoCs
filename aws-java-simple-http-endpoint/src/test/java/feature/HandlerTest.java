533c801a09fed8f3a469b8dab457a802daf74552
leegphillips <leegphillips@yahoo.co.uk>
2017-03-09 11:57:33 +0000

    Created SNS example

--- aws-java-simple-http-endpoint/src/test/java/feature/HandlerTest.java
+++ aws-java-simple-http-endpoint/src/test/java/feature/HandlerTest.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.junit.Cucumber;
 
-// disable this for now as it doesn't fit the patten 
+// disable this for now as it doesn't fit the patten
 //@RunWith(Cucumber.class)
 public class HandlerTest {
 }
