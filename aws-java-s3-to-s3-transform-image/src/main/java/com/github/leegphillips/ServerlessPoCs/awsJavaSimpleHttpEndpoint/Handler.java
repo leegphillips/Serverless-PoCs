b9a87284370e765662efd4ea30919b0542858e40
leegphillips <leegphillips@yahoo.co.uk>
2017-03-22 16:49:28 +0000

    started lambda PoC

---
+++ aws-java-s3-to-s3-transform-image/src/main/java/com/github/leegphillips/ServerlessPoCs/awsJavaSimpleHttpEndpoint/Handler.java
@@ -0,0 +1,31 @@
+package com.github.leegphillips.ServerlessPoCs.awsJavaSimpleHttpEndpoint;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.serverless.ApiGatewayResponse;
+import com.serverless.Response;
+import org.apache.log4j.Logger;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+
+	private static final Logger LOG = Logger.getLogger(Handler.class);
+
+	@Override
+	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+		LOG.info("received: " + input);
+		Response responseBody = new Response(Long.toString(System.currentTimeMillis()));
+		Map<String, String> headers = new HashMap<>();
+		headers.put("X-Powered-By", "AWS Lambda & Serverless");
+		headers.put("Content-Type", "application/json");
+		return ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(headers)
+				.build();
+	}
+}
---
+++ aws-java-s3-to-s3-transform-image/src/test/java/feature/HandlerTest.java
@@ -0,0 +1,10 @@
+package feature;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+// disable this for now as it doesn't fit the patten
+//@RunWith(Cucumber.class)
+public class HandlerTest {
+}
